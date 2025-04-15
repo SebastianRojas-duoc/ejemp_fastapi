6097a0dfa9db81de7aa57ff68e42b52ea2dbfe4a
sebastian Rojas <seb.rojasg@duocuc.cl>
2025-04-15 14:32:23 -0400

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
-
+#importamos la librería para crear la API
 from fastapi import FastAPI
 
+#Vamos a crear una variable para la API:
 api = FastAPI()
 
+#Vamos a crear los endpoints de la API:
 @api.get("/")
 def root():
-    return{"mensaje":"hola mundo api"}
+    return {"mensaje": "Bienvenido desde API 2"}
 
 @api.get("/{id}")
-def getAuto(id : int):
-    autos = {
-        1:{"marca" : "toyota", "modelo" : "supra"},
-        2:{"marca" : "nissan", "modelo" : "v16"}
+def obtener_usuario(id: int):
+    usuarios = {
+        1: {"nombre": "Lalo", "apellido": "Cura"},
+        2: {"nombre": "Elsa", "apellido": "Pato"},
+        3: {"nombre": "Armando", "apellido": "Casas"}
     }
-    return autos.get(id)
+    return usuarios.get(id)
